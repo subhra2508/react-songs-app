6f548f167d2767b250d0dde9b0b85fef35c36d41
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-07-08 09:20:42 +0530

    added songlist component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,15 @@ import { selectSong } from '../actions';
 import SongList from './SongList';
 
 const App = () => {
-    return <div><SongList/></div>
+    return (
+         <div className="ui container grid">
+             <div className="ui row">
+                 <div className="column eight wide">
+                     <SongList/>
+                 </div>
+             </div>
+         </div>
+    )
 };
 
 export default App;
